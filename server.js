c80b7d795598d2eef8dbf02d42a59c0e3266d987
Nate Sampo <nathan.sampo@students.olin.edu>
2020-06-24 03:09:11 -0400

    ok

--- server.js
+++ server.js
@@ -119,11 +119,10 @@ http.createServer(function(request, response) {
 							}
 						}
 
-						response.writeHead(404, {'Content-Type': 'text/html',
+						response.writeHead(200, {'Content-Type': 'text/html',
 												'Access-Control-Allow-Origin': 'herokuapp.com',
 												'Set-Cookie': ['terranovumusername=', 'terranovumauth=']});
-						return response.end();
-						//return response.end(html);
+						return response.end(html);
 					});
 					break;
 				case '/css.css':

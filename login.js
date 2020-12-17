b0de828c91ac4f7b5c1d13d25b2a257e540a0e83
Nate Sampo <nathan.sampo@students.olin.edu>
2020-12-17 16:22:25 -0500

    lots of cleanup and standardization

---
+++ login.js
@@ -0,0 +1,228 @@
+const usernameMaxLength = 16;
+const passwordMaxLength = 32;
+
+function getCookies() {
+	let cookies = {};
+
+	document.cookie.split(';').forEach(function(cookie) {
+		let parts = cookie.split('=');
+		let formatted = parts.shift().trim();
+
+		if (formatted.startsWith('terranovum')) {
+			cookies[formatted] = decodeURIComponent(parts.join('='));
+		}
+	});
+
+	return cookies;
+}
+
+function displayVerify(idToHide) {
+	if (idToHide) {
+		document.getElementById(idToHide).style.display = 'none';
+	}
+	document.getElementById('verify').style.left = '0';
+	document.getElementById('verify').style['line-height'] = '200px';
+	document.getElementById('verify').style['margin-top'] = '-100px';
+	document.getElementById('verify').style.position = 'absolute';
+	document.getElementById('verify').style['text-align'] = 'center';
+	document.getElementById('verify').style.top = '40vh';
+	document.getElementById('verify').style.width = '100vw';
+	document.getElementById('verify').style.display = 'inline';
+}
+
+function displayLogin(idToHide) {
+	if (idToHide) {
+		document.getElementById(idToHide).style.display = 'none';
+	}
+	document.getElementById('login').style.display = 'block';
+	document.getElementById('accountdisplay').style.display = 'none';
+	document.getElementById('accountname').innerHTML = '';
+	document.getElementById('login').style.border = '1px solid black';
+	document.getElementById('login').style.margin = '0 auto';
+	document.getElementById('login').style['margin-top'] = '20vh';
+	document.getElementById('login').style.width = '30vw';
+}
+
+function displayGames(idToHide) {
+	if (idToHide) {
+		document.getElementById(idToHide).style.display = 'none';
+	}
+	document.getElementById('accountdisplay').style.display = 'inline-block';
+	document.getElementById('accountname').innerHTML = getCookies()['terranovumusername'];
+	document.getElementById('games').style.display = 'block';
+	document.getElementById('games').style.border = '1px solid black';
+	document.getElementById('games').style.margin = '0 auto';
+	document.getElementById('games').style['margin-top'] = '10vh';
+	document.getElementById('games').style.width = '30vw';
+}
+
+function login() {
+	const userElem = document.getElementById('usernamelogin');
+	const passElem = document.getElementById('passwordlogin');
+	const userRegElem = document.getElementById('usernamereg');
+	const passRegElem = document.getElementById('passwordreg');
+	const confirmRegElem = document.getElementById('confirmreg');
+
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('POST', '/login', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				userElem.value = '';
+				passElem.value = '';
+				userRegElem.value = '';
+				passRegElem.value = '';
+				confirmRegElem.value = '';
+				displayGames('login');
+			} else if (this.status === 400) {
+				alert('Please ensure all fields are filled in');
+			} else if (this.status === 401) {
+				alert('Incorrect username or password');
+			} else {
+				alert('Uhhh something is wrong');
+			}
+		}
+	}
+	req.send('user=' + encodeURIComponent(userElem.value) + '&pass=' + encodeURIComponent(passElem.value));
+}
+
+function logout() {
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('GET', '/logout', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				displayLogin('games');
+			} else {
+				alert('Uhhh something is wrong');
+			}
+		}
+	}
+	req.send();
+}
+
+function register() {
+	const userElem = document.getElementById('usernamelogin');
+	const passElem = document.getElementById('passwordlogin');
+	const userRegElem = document.getElementById('usernamereg');
+	const passRegElem = document.getElementById('passwordreg');
+	const confirmRegElem = document.getElementById('confirmreg');
+
+	if (userRegElem.value.length > usernameMaxLength) {
+		alert('Username is too long');
+		return;
+	}
+
+	if (userRegElem.value.length == 0) {
+		alert('Username cannot be empty');
+		return;
+	}
+
+	if (passRegElem.value != confirmRegElem.value) {
+		alert('Two different passwords entered');
+		return;
+	}
+
+	if (passRegElem.value.length > passwordMaxLength) {
+		alert('Password too long');
+		return;
+	}
+
+	if (passRegElem.value.length == 0) {
+		alert('Password cannot be empty');
+		return;
+	}
+
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('POST', '/register', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				userElem.value = '';
+				passElem.value = '';
+				userRegElem.value = '';
+				passRegElem.value = '';
+				confirmRegElem.value = '';
+				displayGames('login');
+			} else if (this.status === 400) {
+				alert('Please ensure all fields are filled in');
+			} else if (this.status === 409) {
+				alert('Username Already Taken');
+			} else {
+				alert('Uhhh something is wrong');
+			}
+		}
+	}
+	req.send('user=' + encodeURIComponent(userRegElem.value) + '&pass=' + encodeURIComponent(passRegElem.value));
+}
+
+function getGames() {
+	if (document.cookie.length == 0) {
+		displayLogin();
+
+		return;
+	}
+
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('GET', '/games', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				// TODO
+
+				displayGames('login');
+				//this.responseText
+			} else {
+				alert('Failed to authenticate');
+				displayLogin();
+			}
+		}
+	}
+	req.send();
+}
+
+function createGame() {
+	if (document.cookie.length == 0) {
+		displayLogin();
+
+		return;
+	}
+
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('GET', '/createGame', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				window.location.href = window.location.href + this.response;
+			} else {
+				displayLogin('games');
+				alert('Unable to authenticate');
+			}
+		}
+	}
+	req.send();
+}
+
+if (document.cookie.length > 0) {
+	displayVerify();
+	const req = new XMLHttpRequest();
+	req.withCredentials = true;
+	req.open('GET', '/auth', true);
+	req.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE) {
+			if (this.status === 200) {
+				displayGames('verify');
+			} else {
+				displayLogin('verify');
+			}
+		}
+	}
+	req.send();
+} else {
+	displayLogin();
+}
--- server.js
+++ server.js
@@ -4,54 +4,17 @@ const https = require('https');
 
 const accessToken = '2MMTqs9tilAAAAAAAAAAT12blR8o5M-X26TeYbExYdkM8BT1LxvdUa1y9Tj67Sy';
 
-function getTable(table, func) {
-	const options = {
-		'host': 'content.dropboxapi.com',
-		'path': '/2/files/download',
-		'method': 'POST',
-		'headers': {'Content-Type': 'application/octet-stream',
-					'Authorization': 'Bearer ' + accessToken + 'G',
-					'Dropbox-API-Arg': '{\"path\": \"/tables/' + table + '\"}'}
-	};
-
-	const req = https.request(options, function(response) {
-		if(response.statusCode === 200) {
-			let data = '';
-			response.on('data', function(chunk) {
-				data += chunk;
-			});
-			response.on('end', function() {
-				func(data);
-			});
-		} else {
-			console.log('Dropbox request failed with status: ' + response.statusCode);
+function contains(array, value) {
+	for (var i in array) {
+		if (array[i] == value) {
+			return true;
 		}
-	});
-	req.end();
+	}
+
+	return false;
 }
 
-function saveTable(table, data, func) {
-	const options = {
-		'host': 'content.dropboxapi.com',
-		'path': '/2/files/upload',
-		'method': 'POST',
-		'headers': {'Content-Type': 'application/octet-stream',
-					'Authorization': 'Bearer ' + accessToken + 'G',
-					'Dropbox-API-Arg': '{\"path\": \"/tables/' + table + '\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}'}
-	};
-
-	const req = https.request(options, function(response) {
-		if(response.statusCode === 200) {
-			func();
-		} else {
-			console.log('Dropbox request failed with status: ' + response.statusCode);
-		}
-	});
-	req.write(data);
-	req.end();
-}
-
-function getNewGameNumber(func) {
+function getUserList(func) {
 	const options = {
 		'host': 'api.dropboxapi.com',
 		'path': '/2/files/list_folder',
@@ -67,22 +30,137 @@ function getNewGameNumber(func) {
 				data += chunk;
 			});
 			response.on('end', function() {
+				let users = [];
 				data = JSON.parse(data);
-				let max = 0;
 				for (let i in data['entries']) {
-					const temp = parseInt(data['entries'][i]['name']);
-					if (temp > max) {
-						max = temp;
-					}
+					const temp = data['entries'][i]['name'];
+					users.push(temp);
 				}
 
-				func(max + 1);
+				func(users);
+			});
+		} else {
+			console.log('Dropbox request failed with status: ' + response.statusCode);
+		}
+	});
+	req.write('{\"path\": \"/users/\",\"include_media_info\":false,\"include_deleted\":false}');
+	req.end();
+}
+
+function getUser(name, func) {
+	const options = {
+		'host': 'content.dropboxapi.com',
+		'path': '/2/files/download',
+		'method': 'POST',
+		'headers': {'Content-Type': 'application/octet-stream',
+					'Authorization': 'Bearer ' + accessToken + 'G',
+					'Dropbox-API-Arg': '{\"path\": \"/users/' + name + '\"}'}
+	};
+
+	const req = https.request(options, function(response) {
+		if (response.statusCode === 200) {
+			let data = '';
+			response.on('data', function(chunk) {
+				data += chunk;
+			});
+			response.on('end', function() {
+				func(JSON.parse(data));
+			});
+		} else if (response.statusCode === 409) {
+			func();
+		} else {
+			console.log('Dropbox request failed with status: ' + response.statusCode);
+		}
+	});
+	req.end();
+}
+
+function saveUser(name, data, func) {
+	const options = {
+		'host': 'content.dropboxapi.com',
+		'path': '/2/files/upload',
+		'method': 'POST',
+		'headers': {'Content-Type': 'application/octet-stream',
+					'Authorization': 'Bearer ' + accessToken + 'G',
+					'Dropbox-API-Arg': '{\"path\": \"/users/' + name + '\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}'}
+	};
+
+	const req = https.request(options, function(response) {
+		if(response.statusCode === 200) {
+			func();
+		} else {
+			console.log('Dropbox request failed with status: ' + response.statusCode);
+		}
+	});
+	req.write(data);
+	req.end();
+}
+
+function getGamesList(func) {
+	const options = {
+		'host': 'api.dropboxapi.com',
+		'path': '/2/files/list_folder',
+		'method': 'POST',
+		'headers': {'Content-Type': 'application/json',
+					'Authorization': 'Bearer ' + accessToken + 'G'}
+	};
+
+	const req = https.request(options, function(response) {
+		if(response.statusCode === 200) {
+			let data = '';
+			response.on('data', function(chunk) {
+				data += chunk;
+			});
+			response.on('end', function() {
+				let games = [];
+				data = JSON.parse(data);
+				for (let i in data['entries']) {
+					const temp = data['entries'][i]['name'];
+					games.push(temp);
+				}
+
+				func(games);
 			});
 		} else {
 			console.log('Dropbox request failed with status: ' + response.statusCode);
 		}
 	});
 	req.write('{\"path\": \"/games/\",\"include_media_info\":false,\"include_deleted\":false}');
+	req.end();
+}
+
+function getNewGameNumber(func) {
+	getGamesList(function(data) {
+		let max = 0;
+		for (let i in data) {
+			const temp = parseInt(data[i]);
+			if (temp > max) {
+				max = temp;
+			}
+		}
+
+		func(max + 1);
+	});
+}
+
+function createGame(num, data, func) {
+	const options = {
+		'host': 'content.dropboxapi.com',
+		'path': '/2/files/upload',
+		'method': 'POST',
+		'headers': {'Content-Type': 'application/octet-stream',
+					'Authorization': 'Bearer ' + accessToken + 'G',
+					'Dropbox-API-Arg': '{\"path\": \"/games/' + num + '\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}'}
+	};
+
+	const req = https.request(options, function(response) {
+		if(response.statusCode === 200) {
+			func();
+		} else {
+			console.log('Dropbox request failed with status: ' + response.statusCode);
+		}
+	});
+	req.write(data);
 	req.end();
 }
 
@@ -100,16 +178,14 @@ function authenticateWithCookies(request, response, func) {
 			const name = cookies['terranovumusername'];
 			const cookie = cookies['terranovumauth'];
 			sent = true;
-			getTable('users', function(data) {
-				let parsed = JSON.parse(data);
-				for(let i in parsed) {
-					if(name == parsed[i]['user']) {
-						if(cookie == parsed[i]['cookie']) {
+			getUser(name, function(data) {
+				if (data) {
+					if (name == data['user']) {
+						if (cookie == data['cookie']) {
 							response.writeHead(200, {'Access-Control-Allow-Origin': 'herokuapp.com'});
 							func();
 							return;
 						}
-						break;
 					}
 				}
 
@@ -156,9 +232,9 @@ http.createServer(function(request, response) {
 	switch(request.method) {
 		case 'GET':
 			switch(request.url) {
+				case '':
 				case '/':
 				case '/index.html':
-					let html;
 					fs.readFile('index.html', function(err, data) {
 						if(err) {
 							console.log(err);
@@ -167,8 +243,8 @@ http.createServer(function(request, response) {
 						return response.end(data);
 					});
 					break;
-				case '/css.css':
-					fs.readFile('css.css', function(err, data) {
+				case '/login.css':
+					fs.readFile('login.css', function(err, data) {
 						if(err) {
 							console.log(err);
 						}
@@ -176,8 +252,8 @@ http.createServer(function(request, response) {
 						return response.end(data);
 					});
 					break;
-				case '/game.js':
-					fs.readFile('game.js', function(err, data) {
+				case '/login.js':
+					fs.readFile('login.js', function(err, data) {
 						if(err) {
 							console.log(err);
 						}
@@ -186,11 +262,15 @@ http.createServer(function(request, response) {
 					});
 					break;
 				case '/games':
+					// TODO
 					break;
 				case '/createGame':
 					authenticateWithCookies(request, response, function() {
 						getNewGameNumber(function(data) {
-							return response.end(data.toString());
+							// TODO
+							createGame(data, '', function() {
+								return response.end(data.toString());
+							});
 						});
 					});
 					break;
@@ -203,9 +283,42 @@ http.createServer(function(request, response) {
 					writeClearCookieHeader(response, 200);
 					return response.end();
 					break;
+				case '/404.html':
+					fs.readFile('404.html', function(err, data) {
+						if(err) {
+							console.log(err);
+						}
+						response.writeHeader(404, {'Content-Type': 'text/html'});
+						return response.end(data);
+					});
+					break;
+				case '/404.css':
+					fs.readFile('404.css', function(err, data) {
+						if(err) {
+							console.log(err);
+						}
+						response.writeHeader(200, {'Content-Type': 'text/css'});
+						return response.end(data);
+					});
+					break;
 				default:
-					response.statusCode = 404;
-					return response.end();
+					const pathArr = request.url.split('/');
+					const path = pathArr[pathArr.length - 1];
+
+					getGamesList(function(data) {
+						if(contains(data, path)) {
+							response.writeHeader(200, {'Content-Type': 'text/html'});
+							return response.end();
+						} else {
+							fs.readFile('404.html', function(err, data) {
+								if(err) {
+									console.log(err);
+								}
+								response.writeHeader(404, {'Content-Type': 'text/html'});
+								return response.end(data);
+							});
+						}
+					});
 					break;
 			}
 			break;
@@ -214,7 +327,7 @@ http.createServer(function(request, response) {
 				case '/login':
 					request.on('error', function(err) {
 						console.log(err);
-						response.statusCode = 400;
+						response.statusCode = 500;
 						return response.end();
 					});
 					request.on('data', function(data) {
@@ -224,40 +337,37 @@ http.createServer(function(request, response) {
 						}
 					});
 					request.on('end', function() {
-						getTable('users', function(data) {
-							let parsed = JSON.parse(data);
-							const params = getParams(body);
-							if(params['user'] && params['pass']) {
-								let name = params['user'];
-								let pass = params['pass'];
-								for(let i in parsed) {
-									if(name == parsed[i]['user']) {
-										if(pass == parsed[i]['pass']) {
-											const cookie = createAuthBinaryString();
-											parsed[i]['cookie'] = cookie;
-											saveTable('users', JSON.stringify(parsed), function() {});
-											response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8',
-																	'Access-Control-Allow-Origin': 'herokuapp.com',
-																	'Set-Cookie': ['terranovumusername=' + name, 'terranovumauth=' + cookie]});
-											return response.end();
-										} else {
-											response.statusCode = 401;
-											return response.end();
-										}
-										break;
+						const params = getParams(body);
+						if (params['user'] && params['pass']) {
+							getUser(params['user'], function(data) {
+								if (data) {
+									if (params['pass'] == data['pass']) {
+										const cookie = createAuthBinaryString();
+										data['cookie'] = cookie;
+										saveUser(params['user'], JSON.stringify(data), function() {});
+										response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8',
+																 'Access-Control-Allow-Origin': 'herokuapp.com',
+																 'Set-Cookie': ['terranovumusername=' + params['user'], 'terranovumauth=' + cookie]});
+										return response.end();
+									} else {
+										response.statusCode = 401;
+										return response.end();
 									}
+								} else {
+									response.statusCode = 401;
+									return response.end();
 								}
-							} else {
-								response.statusCode = 400;
-								return response.end();
-							}
-						});
+							});
+						} else {
+							response.statusCode = 400;
+							return response.end();
+						}
 					});
 					break;
 				case '/register':
 					request.on('error', function(err) {
 						console.log(err);
-						response.statusCode = 400;
+						response.statusCode = 500;
 						return response.end();
 					});
 					request.on('data', function(data) {
@@ -267,31 +377,27 @@ http.createServer(function(request, response) {
 						}
 					});
 					request.on('end', function() {
-						getTable('users', function(data) {
-							let parsed = JSON.parse(data);
-							const params = getParams(body);
-							if (params['user'] && params['pass']) {
-								let name = params['user'];
-								let pass = params['pass'];
-								for (let i in parsed) {
-									if (name == parsed[i]['user']) {
+						const params = getParams(body);
+						if (params['user'] && params['pass']) {
+							getUserList(function(data) {
+								for (let i in data) {
+									if (params['user'] == data[i]) {
 										response.statusCode = 409;
 										return response.end();
 									}
 								}
 
 								const cookie = createAuthBinaryString();
-								parsed.push({'user': name, 'pass': pass, 'games': [], 'cookie': cookie});
-								saveTable('users', JSON.stringify(parsed), function() {});
+								saveUser(params['user'], '{\"user\":\"' + params['user'] + '\",\"pass\":\"' + params['pass'] + '\",\"games\":[],\"cookie\":\"' + cookie + '\"}', function() {});
 								response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8',
-														'Access-Control-Allow-Origin': 'herokuapp.com',
-														'Set-Cookie': ['terranovumusername=' + name, 'terranovumauth=' + cookie]});
+														 'Access-Control-Allow-Origin': 'herokuapp.com',
+														 'Set-Cookie': ['terranovumusername=' + params['user'], 'terranovumauth=' + cookie]});
 								return response.end();
-							} else {
-								response.statusCode = 400;
-								return response.end();
-							}
-						});
+							});
+						} else {
+							response.statusCode = 400;
+							return response.end();
+						}
 					});
 					break;
 				default:
